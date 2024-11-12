ebdf748c37c861bf8ff7d608476bdb673590ddca
mohamed <mohamednouaoui5@gmail.com>
2024-11-12 21:31:13 +0100

    created Database in docker

--- src/main/java/com/wora/waitinglistforanyproject/Repository/VisitorRepository.java
+++ src/main/java/com/wora/waitinglistforanyproject/Repository/VisitorRepository.java
@@ -1,6 +1,7 @@
 package com.wora.waitinglistforanyproject.Repository;
 
+import com.wora.waitinglistforanyproject.Entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VisitorRepository extends JpaRepository<VisitorRepository,Long> {
+public interface VisitorRepository extends JpaRepository<Visitor,Long> {
 }
